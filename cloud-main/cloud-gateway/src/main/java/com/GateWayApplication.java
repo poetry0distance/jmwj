98e38731bd93261a2050d4cc3882ca669aebae74
有梦想的好孩子 <718021904@qq.com>
2020-07-29 13:35:52 +0800

    修改新增gatewaydemo

---
+++ cloud-main/cloud-gateway/src/main/java/com/GateWayApplication.java
@@ -0,0 +1,20 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * User: wangtao
+ * Date: 2020/7/29
+ *
+ * @author false
+ * Description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApplication.class,args);
+    }
+}
